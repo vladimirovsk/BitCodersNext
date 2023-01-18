602494d7ccf406004d80d62fa1d54f56556e4e66
Serhii Vladimirov <vladimirovsk@gmail.com>
2023-01-18 11:08:15 -0700

    add Jenkinsfile

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,9 @@ import {Button, Htag} from '../component';
 export default function Home():JSX.Element {
   return (
     <>
-      <Htag tag ='h2' >Htag components</Htag>
-        <Button appearance='primary' className="test">Login</Button>
-        <Button appearance='ghost'>Help</Button>
+      {/*<Htag tag ='h1' >Htag components</Htag>*/}
+      <Button appearance='primary' className="test">Login</Button>
+      <Button appearance='ghost'>Help</Button>
     </>
   )
 }
