796935f749082254740225932a5411247fc8ab4a
Serhii Vladimirov <vladimirovsk@gmail.com>
2023-01-18 13:57:24 -0700

    test commit to Jenkins

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import {Button, Htag} from '../component';
 export default function Home():JSX.Element {
   return (
     <>
-      {/*<Htag tag ='h1' >Htag components</Htag>*/}
+      <Htag tag ='h3' >Htag components</Htag>
       <Button appearance='primary' className="test">Login</Button>
       <Button appearance='ghost'>Help</Button>
     </>
