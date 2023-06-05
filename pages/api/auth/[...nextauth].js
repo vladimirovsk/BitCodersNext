4aacc1dcd822603b405a83ca4b4e8c0c4c8017ef
Serhii Vladimirov <vladimirovsk@gmail.com>
2023-06-05 13:43:05 -0600

    fix google auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,7 +2,7 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
-	site: process.env.NEXTAUTH_URL,
+	// site: process.env.NEXTAUTH_URL,
 	providers: [
 		GoogleProvider({
 			clientId: process.env.GOOGLE_AUTH_CLIENT_ID,
@@ -13,20 +13,22 @@ export default NextAuth({
 		// signIn: '/auth/signin',
 	 // },
 	callbacks: {
+		async redirect({url, baseUrl}){
+			if (url.startsWith("/")) return `${baseUrl}${url}`
+			// Allows callback URLs on the same origin
+			else if (new URL(url).origin === baseUrl) return url
+			console.log('baseUrl',baseUrl)
+			return baseUrl
+		},
 		async session({ session, token, user }) {
-			console.log(session.user)
 			session.user.username = session.user.name
 				.split(' ')
 				.join('')
 				.toLocaleLowerCase()
-
 			session.user.uid = token.sub
-
 			return session
 		},
 	},
 	secret: process.env.NEXT_PUBLIC_SECRET,
-	// session: {
-	// 	strategy: 'jwt'
-	// }
+	debug: true,
 })
