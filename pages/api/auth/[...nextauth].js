4e00457ef1a1a9f508d603e435c09e7382fb36ef
Serhii Vladimirov <vladimirovsk@gmail.com>
2023-06-05 17:13:43 -0600

    fix google auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -17,7 +17,7 @@ export default NextAuth({
 			// if (url.startsWith("/")) return `${baseUrl}${url}`
 			// // Allows callback URLs on the same origin
 			// else if (new URL(url).origin === baseUrl) return url
-			baseUrl = process.env.NEXTAUTH_URL;
+			baseUrl = process.env.NEXTAPI_URL;
 			console.log('baseUrl',baseUrl)
 			return baseUrl
 		},
