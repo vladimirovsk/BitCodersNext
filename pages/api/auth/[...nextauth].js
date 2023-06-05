0cfcf9f5ad1c8aae0d0c0d2099eb80f9955028f3
Serhii Vladimirov <vladimirovsk@gmail.com>
2023-06-05 10:59:05 -0600

    fix google auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
+	site: process.env.NEXTAUTH_URL,
 	providers: [
 		GoogleProvider({
 			clientId: process.env.GOOGLE_AUTH_CLIENT_ID,
