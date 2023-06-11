0d79a9632552f992dfeb2983fbdbf63d317a9e95
Serhii Vladimirov <vladimirovsk@gmail.com>
2023-06-11 12:26:21 -0600

    fix google auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,7 +3,6 @@ import GoogleProvider from "next-auth/providers/google";
 
 
 export default NextAuth({
-	// site: process.env.NEXTAUTH_URL,
 	providers: [
 		GoogleProvider({
 			clientId: String(process.env.GOOGLE_AUTH_CLIENT_ID),
@@ -14,16 +13,17 @@ export default NextAuth({
 		// signIn: '/auth/signin',
 	 // },
 	callbacks: {
-		 //async redirect({url, baseUrl}){
-		// 	// if (url.startsWith("/")) return `${baseUrl}${url}`
-		// 	// // Allows callback URLs on the same origin
-		// 	// else if (new URL(url).origin === baseUrl) return url
+		 async redirect({url, baseUrl}){
+			// if (url.startsWith("/")) return `${baseUrl}${url}`
+			// // Allows callback URLs on the same origin
+			// else if (new URL(url).origin === baseUrl) return url
+		    console.log(process.env.NEXTAUTH_URL, process.env.NEXT_PUBLIC_SECRET)
 		 	//baseUrl = process.env.NEXTAUTH_URL
-			//console.log('baseUrl', baseUrl, process.env.NEXTAUTH_URL)
-			//return baseUrl
-		 //},
+			console.log('baseUrl', baseUrl, process.env.NEXTAUTH_URL)
+			return baseUrl
+		 },
 		async session ({ session, token, user }) {
-			console.log(process.env.NEXTAUTH_URL, process.env.NEXT_PUBLIC_SECRET)
+
 			// session.user.username = session.user.name
 			// 	.split(' ')
 			// 	.join('')
