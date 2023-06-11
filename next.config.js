b0bc36f615ba6cbaec01603b1afee478e0e6ab57
Serhii Vladimirov <vladimirovsk@gmail.com>
2023-06-11 12:32:42 -0600

    fix google auth

--- next.config.js
+++ next.config.js
@@ -7,10 +7,10 @@ const nextConfig = {
     domains: ['dev.bitcoders.net']
   },
   env: {
+    GOOGLE_AUTH_CLIENT_ID: process.env.GOOGLE_AUTH_CLIENT_ID,
+    GOOGLE_AUTH_CLIENT_SECRET: process.env.GOOGLE_AUTH_CLIENT_SECRET,
     NEXT_PUBLIC_SECRET: process.env.NEXT_PUBLIC_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    VERCEL: process.env.NEXTAUTH_URL,
-    VERCEL_URL: process.env.NEXTAUTH_URL
   }
 };
 
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -9,16 +9,9 @@ export default NextAuth({
 			clientSecret: String(process.env.GOOGLE_AUTH_CLIENT_SECRET),
 		})
 	],
-	 // pages: {
-		// signIn: '/auth/signin',
-	 // },
 	callbacks: {
 		 async redirect({url, baseUrl}){
-			// if (url.startsWith("/")) return `${baseUrl}${url}`
-			// // Allows callback URLs on the same origin
-			// else if (new URL(url).origin === baseUrl) return url
 		    console.log(process.env.NEXTAUTH_URL, process.env.NEXT_PUBLIC_SECRET)
-		 	//baseUrl = process.env.NEXTAUTH_URL
 			console.log('baseUrl', baseUrl, process.env.NEXTAUTH_URL)
 			return baseUrl
 		 },
