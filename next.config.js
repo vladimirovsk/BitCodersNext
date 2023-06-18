79918cf5d3832b71ef676f6c5f9e2ed9953214fe
Serhii Vladimirov <vladimirovsk@gmail.com>
2023-06-17 21:27:35 -0600

    fix google auth

--- next.config.js
+++ next.config.js
@@ -7,9 +7,8 @@ const nextConfig = {
     domains: ['dev.bitcoders.net']
   },
   env: {
-    GOOGLE_AUTH_CLIENT_ID: process.env.GOOGLE_AUTH_CLIENT_ID,
-    GOOGLE_AUTH_CLIENT_SECRET: process.env.GOOGLE_AUTH_CLIENT_SECRET,
-    NEXT_PUBLIC_SECRET: process.env.NEXT_PUBLIC_SECRET,
+    GOOGLE_ID: process.env.GOOGLE_AUTH_CLIENT_ID,
+    GOOGLE_SECRET: process.env.GOOGLE_AUTH_CLIENT_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET
   }
